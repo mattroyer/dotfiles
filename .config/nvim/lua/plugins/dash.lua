238079cdedd759549c73f74f76d43fa41f7247cb
Matt Royer <matthew.royer@gmail.com>
2024-03-21 12:00:24 -0600

    Update dash.lua

--- .config/nvim/lua/plugins/dash.lua
+++ .config/nvim/lua/plugins/dash.lua
@@ -1,8 +1,8 @@
-banners_directory = vim.fn.stdpath("config") .. "/lua/plugins/banners/"
+BANNERS_DIRECTORY = vim.fn.stdpath("config") .. "/lua/plugins/banners/"
 
 local function read_banner_from_file(filename)
   local lines = {}
-  for line in io.lines(banners_directory .. filename) do
+  for line in io.lines(BANNERS_DIRECTORY .. filename) do
     table.insert(lines, line)
   end
   return lines
@@ -18,7 +18,7 @@ local function random_banner()
   math.randomseed(os.time())
 
   -- Let's grab all the files in the banners directory
-  local banner_paths = vim.fn.glob(banners_directory .. "*.txt", false, true)
+  local banner_paths = vim.fn.glob(BANNERS_DIRECTORY .. "*.txt", false, true)
   local banners = {}
   for _, path in ipairs(banner_paths) do
     table.insert(banners, extract_filename(path))
@@ -34,6 +34,6 @@ return {
     "nvimdev/dashboard-nvim",
     opts = function(_, opts)
       opts.config.header = random_banner()
-    end
-  }
+    end,
+  },
 }
